a7359202bff54d74dbb5f3e60b2c54b09ce9cf87
Kris Zima <kris@stashproperty.com.au>
2024-01-01 23:15:56 +0100

    add redirect from demo to edit page

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: "/demo",
+        destination: "/edit",
+        permanent: true,
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
