bca5d8a134598c63cb2155be5f52414b6f115c47
Kris Zima <kris@stashproperty.com.au>
2024-01-18 08:35:07 +0100

    make pricing readable by all users

--- supabase/migrations/20231216164750_add_polices.sql
+++ supabase/migrations/20231216164750_add_polices.sql
@@ -1,6 +1,7 @@
 ALTER TABLE public.pricing ENABLE ROW LEVEL SECURITY;
 
+CREATE POLICY select_for_all_users ON public.pricing FOR SELECT USING (true);
+
 CREATE POLICY insert_for_authenticated ON public.pricing FOR INSERT TO authenticated WITH CHECK (true);
-CREATE POLICY select_for_authenticated ON public.pricing FOR SELECT TO authenticated USING (true);
 CREATE POLICY update_for_authenticated ON public.pricing FOR UPDATE TO authenticated USING (true);
 CREATE POLICY delete_for_authenticated ON public.pricing FOR DELETE TO authenticated USING (true);
